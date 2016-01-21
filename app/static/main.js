dc09ea455ddabe6ef452f09b4d41a1093de9ce83
AuTa <auta520@live.com>
2016-01-21 17:32:59 +0900

    Kana basic web page.
    add main.css main.js
    kana.py add more kana
    kana.html add macro kana-table

---
+++ app/static/main.js
@@ -0,0 +1,51 @@
+$(".kana").click(function() {
+  $(this).toggleClass("selected");
+});
+
+$(".kana").hover(function() {
+  $(this).toggleClass("hover");
+}, function() {
+  $(this).toggleClass("hover");
+});
+
+$(".sel-but").click(function() {
+  var i = $(this).data("cell");
+  var j = $(this).data("row");
+  if (j === 0) {
+    $(this).parents("table").find(".kana[data-cell='" + i + "'][data-row!='0']").toggleClass("selected");
+  };
+  if (i === 0) {
+    $(this).parents("table").find(".kana[data-cell!='0'][data-row='" + j + "']").toggleClass("selected");
+  };
+});
+
+$(".sel-but").hover(function() {
+  var i = $(this).data("cell");
+  var j = $(this).data("row");
+  if (j === 0) {
+    $(this).parents("table").find(".kana[data-cell='" + i + "'][data-row!='0']").toggleClass("hover");
+  };
+  if (i === 0) {
+    $(this).parents("table").find(".kana[data-cell!='0'][data-row='" + j + "']").toggleClass("hover");
+  };
+});
+
+$(".sel-but[data-cell='0'][data-row='0']").click(function() {
+  $(this).parents("table").find(".kana").toggleClass("selected");
+});
+
+$(".sel-but[data-cell='0'][data-row='0']").hover(function() {
+  $(this).parents("table").find(".kana").toggleClass("hover");
+});
+
+$(".sel-but[data-cell='0'][data-row='0']").dblclick(function() {
+  $(this).parents("table").find(".kana").addClass("selected");
+});
+
+$(document).ready(function() {
+    if (window.matchMedia('(min-width: 750px)').matches) {
+        $(".tab-pane").addClass("active");
+    } else {
+        //...
+    }
+});
