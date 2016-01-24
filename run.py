bd06cf289c6166e8ef6b31a02ee85fcdb6a7d1eb
AuTa <auta520@live.com>
2016-01-25 00:53:04 +0900

    Add shell kana pok.

--- run.py
+++ run.py
@@ -3,15 +3,17 @@
 import os
 from app import create_app, db
 from flask_script import Manager, Shell
-
+from app.models import Kana, PronunciationOfKanamoji
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'development')
 db = db
+kana = Kana
+pok = PronunciationOfKanamoji
 manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, kana=kana, pok=pok)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
@@ -28,3 +30,4 @@ def test():
 
 if __name__ == '__main__':
     manager.run()
+
