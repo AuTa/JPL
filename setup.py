bc7c9bc6cfdc44936cafc399c470740a51b54e78
AuTa <auta520@live.com>
2016-01-29 09:46:15 +0900

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup(
+    name='JPL',
+    version='0.0.1',
+    packages=['app', 'app.views', 'test', 'config'],
+    url='',
+    license='',
+    author='AuTa',
+    author_email='auta520@outlook.com',
+    description=''
+)
